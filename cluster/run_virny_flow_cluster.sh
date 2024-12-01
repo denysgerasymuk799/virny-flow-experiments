40706d792f0e164d0dfae2160e5ef8c9275bfa8c
Denys Herasymuk <herasymuk@ucu.edu.ua>
2024-12-01 13:23:38 +0200

    Prepared run_virny_flow_cluster.sh

--- cluster/run_virny_flow_cluster.sh
+++ cluster/run_virny_flow_cluster.sh
@@ -2,12 +2,16 @@
 
 num_workers=$1
 
+singularity exec --overlay /scratch/dh3553/virny_flow_project/vldb_sds_env.ext3:ro /scratch/work/public/singularity/ubuntu-20.04.1.sif /bin/bash
+
+source /ext3/env.sh
+
 # Start the task manager
-python ../scripts/run_task_manager.py &
+python ../scripts/run_task_manager.py > ./task_manager.txt 2>&1 &
 
 # Start worker processes
 for i in {1..$num_workers}; do
-    python ../scripts/run_worker.py &
+    python ../scripts/run_worker.py > ./worker_$num_workers.txt 2>&1 &
 done
 
 # Wait for all background processes to finish
