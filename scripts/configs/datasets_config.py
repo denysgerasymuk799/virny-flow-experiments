791225250a451233a8a6e274250d1c8dd77d8eb8
Denys Herasymuk <herasymuk@ucu.edu.ua>
2024-12-16 19:29:38 +0200

    Prepared a repo for experiment 1

--- scripts/configs/datasets_config.py
+++ scripts/configs/datasets_config.py
@@ -1,26 +1,38 @@
-import pathlib
-
-from virny.datasets import DiabetesDataset2019, GermanCreditDataset, ACSEmploymentDataset
+from virny.datasets import (DiabetesDataset2019, GermanCreditDataset, ACSEmploymentDataset, ACSIncomeDataset,
+                            LawSchoolDataset, CardiovascularDiseaseDataset)
 
 
 DATASET_CONFIG = {
-    "german": {
-        "data_loader": GermanCreditDataset,
-        "data_loader_kwargs": {},
-        "test_set_fraction": 0.3,
-        "virny_config_path": pathlib.Path(__file__).parent.joinpath('yaml_files', 'german_config.yaml')
-    },
     "diabetes": {
         "data_loader": DiabetesDataset2019,
         "data_loader_kwargs": {'with_nulls': False},
         "test_set_fraction": 0.3,
-        "virny_config_path": pathlib.Path(__file__).parent.joinpath('yaml_files', 'diabetes_config.yaml')
+    },
+    "german": {
+        "data_loader": GermanCreditDataset,
+        "data_loader_kwargs": {},
+        "test_set_fraction": 0.3,
     },
     "folk_emp": {
         "data_loader": ACSEmploymentDataset,
         "data_loader_kwargs": {"state": ['CA'], "year": 2018, "with_nulls": False,
                                "subsample_size": 15_000, "subsample_seed": 42},
         "test_set_fraction": 0.2,
-        "virny_config_path": pathlib.Path(__file__).parent.joinpath('yaml_files', 'folk_emp_config.yaml')
+    },
+    "folk_inc": {
+        "data_loader": ACSIncomeDataset,
+        "data_loader_kwargs": {"state": ['GA'], "year": 2018, "with_nulls": False,
+                               "subsample_size": 15_000, "subsample_seed": 42},
+        "test_set_fraction": 0.2,
+    },
+    "law_school": {
+        "data_loader": LawSchoolDataset,
+        "data_loader_kwargs": {},
+        "test_set_fraction": 0.2,
+    },
+    "heart": {
+        "data_loader": CardiovascularDiseaseDataset,
+        "data_loader_kwargs": {},
+        "test_set_fraction": 0.2,
     },
 }
