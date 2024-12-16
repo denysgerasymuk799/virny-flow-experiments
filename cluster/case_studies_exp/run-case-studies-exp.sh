fa3a349133db6d913e48ce78ec4a01e38c075dee
Denys Herasymuk <herasymuk@ucu.edu.ua>
2024-12-16 20:54:23 +0200

    Prepared all files for diabetes and exp1

--- cluster/case_studies_exp/run-case-studies-exp.sh
+++ cluster/case_studies_exp/run-case-studies-exp.sh
@@ -1,6 +1,72 @@
 # Define the list of tuples (exp_name, dataset, case_study, run_num, w1, w2, w3, exp_config_suffix, max_trials, email)
 declare -a job_configs=(
-    "case_studies_exp diabetes cs1 1 0.5 0.5 0.5 w_acc_0_5_w_fair_0_5_w_stab_0_5 200 dh3553"
+    # Case study 1
+    "case_studies_exp diabetes cs1 1 0.5 0.5 0.0 w_acc_0_5_w_fair_0_5 200 dh3553"
+    "case_studies_exp diabetes cs1 1 0.25 0.75 0.0 w_acc_0_25_w_fair_0_75 200 dh3553"
+    "case_studies_exp diabetes cs1 1 0.75 0.25 0.0 w_acc_0_75_w_fair_0_25 200 dh3553"
+
+    "case_studies_exp diabetes cs1 2 0.5 0.5 0.0 w_acc_0_5_w_fair_0_5 200 dh3553"
+    "case_studies_exp diabetes cs1 2 0.25 0.75 0.0 w_acc_0_25_w_fair_0_75 200 dh3553"
+    "case_studies_exp diabetes cs1 2 0.75 0.25 0.0 w_acc_0_75_w_fair_0_25 200 dh3553"
+
+    "case_studies_exp diabetes cs1 3 0.5 0.5 0.0 w_acc_0_5_w_fair_0_5 200 dh3553"
+    "case_studies_exp diabetes cs1 3 0.25 0.75 0.0 w_acc_0_25_w_fair_0_75 200 dh3553"
+    "case_studies_exp diabetes cs1 3 0.75 0.25 0.0 w_acc_0_75_w_fair_0_25 200 dh3553"
+
+    "case_studies_exp diabetes cs1 4 0.5 0.5 0.0 w_acc_0_5_w_fair_0_5 200 dh3553"
+    "case_studies_exp diabetes cs1 4 0.25 0.75 0.0 w_acc_0_25_w_fair_0_75 200 dh3553"
+    "case_studies_exp diabetes cs1 4 0.75 0.25 0.0 w_acc_0_75_w_fair_0_25 200 dh3553"
+
+    "case_studies_exp diabetes cs1 5 0.5 0.5 0.0 w_acc_0_5_w_fair_0_5 200 dh3553"
+    "case_studies_exp diabetes cs1 5 0.25 0.75 0.0 w_acc_0_25_w_fair_0_75 200 dh3553"
+    "case_studies_exp diabetes cs1 5 0.75 0.25 0.0 w_acc_0_75_w_fair_0_25 200 dh3553"
+
+    # Case study 2
+    "case_studies_exp diabetes cs2 1 0.5 0.5 0.0 w_acc_0_5_w_stab_0_5 200 dh3553"
+    "case_studies_exp diabetes cs2 1 0.25 0.75 0.0 w_acc_0_25_w_stab_0_75 200 dh3553"
+    "case_studies_exp diabetes cs2 1 0.75 0.25 0.0 w_acc_0_75_w_stab_0_25 200 dh3553"
+
+    "case_studies_exp diabetes cs2 2 0.5 0.5 0.0 w_acc_0_5_w_stab_0_5 200 dh3553"
+    "case_studies_exp diabetes cs2 2 0.25 0.75 0.0 w_acc_0_25_w_stab_0_75 200 dh3553"
+    "case_studies_exp diabetes cs2 2 0.75 0.25 0.0 w_acc_0_75_w_stab_0_25 200 dh3553"
+
+    "case_studies_exp diabetes cs2 3 0.5 0.5 0.0 w_acc_0_5_w_stab_0_5 200 dh3553"
+    "case_studies_exp diabetes cs2 3 0.25 0.75 0.0 w_acc_0_25_w_stab_0_75 200 dh3553"
+    "case_studies_exp diabetes cs2 3 0.75 0.25 0.0 w_acc_0_75_w_stab_0_25 200 dh3553"
+
+    "case_studies_exp diabetes cs2 4 0.5 0.5 0.0 w_acc_0_5_w_stab_0_5 200 dh3553"
+    "case_studies_exp diabetes cs2 4 0.25 0.75 0.0 w_acc_0_25_w_stab_0_75 200 dh3553"
+    "case_studies_exp diabetes cs2 4 0.75 0.25 0.0 w_acc_0_75_w_stab_0_25 200 dh3553"
+
+    "case_studies_exp diabetes cs2 5 0.5 0.5 0.0 w_acc_0_5_w_stab_0_5 200 dh3553"
+    "case_studies_exp diabetes cs2 5 0.25 0.75 0.0 w_acc_0_25_w_stab_0_75 200 dh3553"
+    "case_studies_exp diabetes cs2 5 0.75 0.25 0.0 w_acc_0_75_w_stab_0_25 200 dh3553"
+
+    # Case study 3
+    "case_studies_exp diabetes cs3 1 0.33 0.33 0.33 w_acc_0_33_w_fair_0_33_w_stab_0_33 200 dh3553"
+    "case_studies_exp diabetes cs3 1 0.5 0.25 0.25 w_acc_0_5_w_fair_0_25_w_stab_0_25 200 dh3553"
+    "case_studies_exp diabetes cs3 1 0.25 0.5 0.25 w_acc_0_25_w_fair_0_5_w_stab_0_25 200 dh3553"
+    "case_studies_exp diabetes cs3 1 0.25 0.25 0.5 w_acc_0_25_w_fair_0_25_w_stab_0_5 200 dh3553"
+
+    "case_studies_exp diabetes cs3 2 0.33 0.33 0.33 w_acc_0_33_w_fair_0_33_w_stab_0_33 200 dh3553"
+    "case_studies_exp diabetes cs3 2 0.5 0.25 0.25 w_acc_0_5_w_fair_0_25_w_stab_0_25 200 dh3553"
+    "case_studies_exp diabetes cs3 2 0.25 0.5 0.25 w_acc_0_25_w_fair_0_5_w_stab_0_25 200 dh3553"
+    "case_studies_exp diabetes cs3 2 0.25 0.25 0.5 w_acc_0_25_w_fair_0_25_w_stab_0_5 200 dh3553"
+
+    "case_studies_exp diabetes cs3 3 0.33 0.33 0.33 w_acc_0_33_w_fair_0_33_w_stab_0_33 200 dh3553"
+    "case_studies_exp diabetes cs3 3 0.5 0.25 0.25 w_acc_0_5_w_fair_0_25_w_stab_0_25 200 dh3553"
+    "case_studies_exp diabetes cs3 3 0.25 0.5 0.25 w_acc_0_25_w_fair_0_5_w_stab_0_25 200 dh3553"
+    "case_studies_exp diabetes cs3 3 0.25 0.25 0.5 w_acc_0_25_w_fair_0_25_w_stab_0_5 200 dh3553"
+
+    "case_studies_exp diabetes cs3 4 0.33 0.33 0.33 w_acc_0_33_w_fair_0_33_w_stab_0_33 200 dh3553"
+    "case_studies_exp diabetes cs3 4 0.5 0.25 0.25 w_acc_0_5_w_fair_0_25_w_stab_0_25 200 dh3553"
+    "case_studies_exp diabetes cs3 4 0.25 0.5 0.25 w_acc_0_25_w_fair_0_5_w_stab_0_25 200 dh3553"
+    "case_studies_exp diabetes cs3 4 0.25 0.25 0.5 w_acc_0_25_w_fair_0_25_w_stab_0_5 200 dh3553"
+
+    "case_studies_exp diabetes cs3 5 0.33 0.33 0.33 w_acc_0_33_w_fair_0_33_w_stab_0_33 200 dh3553"
+    "case_studies_exp diabetes cs3 5 0.5 0.25 0.25 w_acc_0_5_w_fair_0_25_w_stab_0_25 200 dh3553"
+    "case_studies_exp diabetes cs3 5 0.25 0.5 0.25 w_acc_0_25_w_fair_0_5_w_stab_0_25 200 dh3553"
+    "case_studies_exp diabetes cs3 5 0.25 0.25 0.5 w_acc_0_25_w_fair_0_25_w_stab_0_5 200 dh3553"
 )
 
 # Initialize a counter
