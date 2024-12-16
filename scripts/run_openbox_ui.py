596e7fbc1f05f16a053d901a2781696497a9a33d
Denys Herasymuk <herasymuk@ucu.edu.ua>
2024-12-16 10:45:45 +0200

    Prepared sbatch files for wo openbox weights

--- scripts/run_openbox_ui.py
+++ scripts/run_openbox_ui.py
@@ -1,112 +1,17 @@
 import pathlib
-from typing import Union
 from openbox import History
-from openbox.visualization.base_visualizer import _parse_option, NullVisualizer
-from openbox.utils.config_space import ConfigurationSpace
 
-from virny_flow.core.utils.common_helpers import create_exp_config_obj
-from virny_flow.configs.constants import StageName, NO_FAIRNESS_INTERVENTION, STAGE_SEPARATOR
 from virny_flow.configs.structs import BOAdvisorConfig
-from virny_flow.configs.component_configs import (NULL_IMPUTATION_CONFIG, FAIRNESS_INTERVENTION_CONFIG_SPACE,
-                                                  get_models_params_for_tuning)
-
-
-def build_visualizer(
-        option: Union[str, bool],
-        history: History,
-        *,
-        logging_dir='logs/',
-        task_info=None,
-        **kwargs,
-):
-    """
-    Build visualizer for optimizer.
-
-    Parameters
-    ----------
-    option : ['none', 'basic', 'advanced']
-        Visualizer option.
-    history : History
-        History to visualize.
-    logging_dir : str, default='logs/'
-        The directory to save the visualization.
-    task_info : dict, optional
-        Task information for visualizer to use.
-    optimizer : Optimizer, optional
-        Optimizer to extract task_info from.
-    advisor : Advisor, optional
-        Advisor to extract task_info from.
-    kwargs : dict
-        Other arguments for visualizer.
-        For HTMLVisualizer, available arguments are:
-        - auto_open_html : bool, default=False
-            Whether to open html file automatically.
-        - advanced_analysis_options : dict, default=None
-            Advanced analysis options. See `HTMLVisualizer` for details.
-
-    Returns
-    -------
-    visualizer : BaseVisualizer
-        Visualizer.
-    """
-    option = _parse_option(option)
-
-    if option == 'none':
-        visualizer = NullVisualizer()
-    elif option in ['basic', 'advanced']:
-        from openbox.visualization.html_visualizer import HTMLVisualizer
-        visualizer = HTMLVisualizer(
-            logging_dir=logging_dir,
-            history=history,
-            task_info=task_info,
-            auto_open_html=kwargs.get('auto_open_html', False),
-            advanced_analysis=(option == 'advanced'),
-            advanced_analysis_options=kwargs.get('advanced_analysis_options'),
-        )
-    else:
-        raise ValueError('Unknown visualizer option: %s' % option)
-
-    return visualizer
-
-
-def create_config_space(logical_pipeline_name: str):
-    # Create a config space based on config spaces of each stage
-    config_space = ConfigurationSpace()
-    lp_stages = logical_pipeline_name.split(STAGE_SEPARATOR)
-    components = {
-        StageName.null_imputation.value: lp_stages[0],
-        StageName.fairness_intervention.value: lp_stages[1],
-        StageName.model_evaluation.value: lp_stages[2],
-    }
-    for stage in StageName:
-        if stage == StageName.null_imputation:
-            # None imputer does not have config space
-            if components[stage.value] == 'None':
-                continue
-
-            stage_config_space = NULL_IMPUTATION_CONFIG[components[stage.value]]['config_space']
-        elif stage == StageName.fairness_intervention:
-            # NO_FAIRNESS_INTERVENTION does not have config space
-            if components[stage.value] == NO_FAIRNESS_INTERVENTION:
-                continue
-
-            stage_config_space = FAIRNESS_INTERVENTION_CONFIG_SPACE[components[stage.value]]
-        else:
-            # We set seed to None since we need only a config space
-            stage_config_space = get_models_params_for_tuning(models_tuning_seed=None)[components[stage.value]]['config_space']
-
-        config_space.add_hyperparameters(list(stage_config_space.values()))
-
-    return config_space
+from virny_flow.core.utils.common_helpers import create_exp_config_obj
+from virny_flow.visualizations.viz_utils import build_visualizer, create_config_space
 
 
 if __name__ == '__main__':
     # Input variables
-    exp_config_name = "cost_model_exp1_folk_emp_w_acc_1_0_w_fair_0_0"
+    lp_name = 'miss_forest&DIR&lr_clf'
     run_num = 1
-    lp_name = 'None&DIR&lgbm_clf'
-    history_filename = 'history_2024-12-04-08-15-25-655409.json'
-    surrogate_model_type = 'gp'  # 'gp' or 'prf'
+    history_filename = 'history_2024-12-14-23-54-45-330002.json'
+    surrogate_model_type = 'prf'  # 'gp' or 'prf'
     bo_advisor_config = BOAdvisorConfig()
 
     # Read an experimental config
@@ -114,7 +19,7 @@ if __name__ == '__main__':
     exp_config = create_exp_config_obj(exp_config_yaml_path=exp_config_yaml_path)
 
     config_space = create_config_space(lp_name)
-    history_path = f'../logs/history/{exp_config_name}/run_num_{str(run_num)}/{lp_name}/' + history_filename
+    history_path = f'../history/{exp_config.common_args.exp_config_name}/run_num_{str(run_num)}/{lp_name}/' + history_filename
     history = History.load_json(history_path, config_space)
 
     task_info = {
